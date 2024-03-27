22e199d43eac93f78f3b95cc211fd940ed90a325
Kinga Lenart <kingalenartt@gmail.com>
2024-03-27 20:08:11 +0100

    Add project files.

---
+++ StackOverflowTagApp.Core.SQL/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StackOverflowTagApp.Core.Models;
+
+namespace StackOverflowTagApp.Core.SQL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<TypicalTagEntity> Tags { get; set; }
+        public DbSet<UserTagEntity> UserTags { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) 
+        { }
+    }
+}
---
+++ StackOverflowTagApp.Core.Services/StackOverflowReadTagsService.cs
@@ -0,0 +1,24 @@
+﻿using StackOverflowTagApp.Core.SQL;
+
+namespace StackOverflowTagApp.Core.Services
+{
+    public class StackOverflowReadTagsService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ApplicationDbContext _context;
+        public StackOverflowReadTagsService(HttpClient httpClient, ApplicationDbContext context)
+        {
+            _httpClient = httpClient;
+            _context = context;
+        }
+
+        public async Task FetchTagsAsync()
+        {
+            string url = "https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow";
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+        }
+    }
+
+}
