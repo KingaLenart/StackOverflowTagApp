716a35cf25e45be9b3ac2d28d8c43b56aed87d8b
Kinga Lenart <kingalenartt@gmail.com>
2024-04-05 12:51:27 +0200

    Add integration tests.

--- StackOverflowTagApp.Api/Program.cs
+++ StackOverflowTagApp.Api/Program.cs
@@ -4,90 +4,95 @@ using StackOverflowTagApp.Core.SQL;
 using System.Net;
 using StackOverflowTagApp.Core.Application.DI;
 using StackOverflowTagApp.Core.Infrastructure.ExceptionHandling;
-using Microsoft.Data.SqlClient;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-// Add connection to SQL
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("SQL"), sqlOptionsBuilder => 
+public partial class Program
 {
-    sqlOptionsBuilder.MigrationsAssembly("StackOverflowTagApp.Core.SQL");
-    sqlOptionsBuilder.CommandTimeout((int)TimeSpan.FromMinutes(10).TotalSeconds);
-    sqlOptionsBuilder.UseQuerySplittingBehavior(QuerySplittingBehavior.SingleQuery);
-}));
-
-// HttpClient Registration
-
-builder.Services.AddHttpClient("StackOverflowClient", c =>
-{
-    c.BaseAddress = new Uri("https://api.stackexchange.com/2.3/");
-}).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
-{
-    AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
-});
-builder.Services.AddStackOverflowServices();
-builder.Services.AddCoreApplicationServices();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// GlobalExceptionHandler
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-builder.Services.AddProblemDetails();
-
-var app = builder.Build();
-
-
-await EnsureDatabaseReadyAndMigrate();
-
-// HTTP request pipeline configuration.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseExceptionHandler();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
-
-// Docker compose - ensures API waits for SQL Server readiness, preventing startup errors
-async Task EnsureDatabaseReadyAndMigrate()
-{
-    using var scope = app.Services.CreateScope();
-
-    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-
-    var timeOut = TimeSpan.FromSeconds(30);
-    var migrated = false;
-    var startTime = DateTime.UtcNow;
-    while (!migrated)
+    public static async Task Main(string[] args)
     {
-        if (DateTime.UtcNow - startTime > timeOut)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+
+        // Add connection to SQL
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("SQL"), sqlOptionsBuilder =>
         {
-            throw new TimeoutException("Timeout waiting for database to become available.");
+            sqlOptionsBuilder.MigrationsAssembly("StackOverflowTagApp.Core.SQL");
+            sqlOptionsBuilder.CommandTimeout((int)TimeSpan.FromMinutes(10).TotalSeconds);
+            sqlOptionsBuilder.UseQuerySplittingBehavior(QuerySplittingBehavior.SingleQuery);
+        }));
+
+        // HttpClient Registration
+
+        builder.Services.AddHttpClient("StackOverflowClient", c =>
+        {
+            c.BaseAddress = new Uri("https://api.stackexchange.com/2.3/");
+        }).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+        {
+            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+        });
+        builder.Services.AddStackOverflowServices();
+        builder.Services.AddCoreApplicationServices();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        // GlobalExceptionHandler
+        builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+        builder.Services.AddProblemDetails();
+
+        var app = builder.Build();
+
+
+        await EnsureDatabaseReadyAndMigrate();
+
+        // HTTP request pipeline configuration.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
-        try
+
+        app.UseExceptionHandler();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+
+        // Docker compose - ensures API waits for SQL Server readiness, preventing startup errors
+        async Task EnsureDatabaseReadyAndMigrate()
         {
-            dbContext.Database.Migrate();
-            migrated = true;
-        }
-        catch (Exception)
-        {
-            Console.WriteLine("Waiting for database to become available...");
-            await Task.Delay(5000);
+            using var scope = app.Services.CreateScope();
+
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            var timeOut = TimeSpan.FromSeconds(30);
+            var migrated = false;
+            var startTime = DateTime.UtcNow;
+            while (!migrated)
+            {
+                if (DateTime.UtcNow - startTime > timeOut)
+                {
+                    throw new TimeoutException("Timeout waiting for database to become available.");
+                }
+                try
+                {
+                    dbContext.Database.Migrate();
+                    migrated = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Waiting for database to become available...");
+                    await Task.Delay(5000);
+                }
+            }
         }
     }
 }
---
+++ StackOverflowTagApp.Core.IntegrationTests/Controllers/TagControllerTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using StackOverflowTagApp.Core.Application.Models;
+using StackOverflowTagApp.Core.Domain;
+using StackOverflowTagApp.Core.Infrastructure.StackOverflow.Models;
+using System.Net;
+using System.Text;
+
+namespace StackOverflowTagApp.Core.IntegrationTests.Controllers;
+
+[TestFixture]
+public class TagControllerTests
+{
+    private WebApplicationFactory<Program> _factory;
+    private HttpClient _client;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _factory = new WebApplicationFactory<Program>();
+        _client = _factory.CreateClient();
+    }
+
+    [Test]
+    public async Task SyncTags_EndpointReturnsSuccess()
+    {
+        // Act
+        var response = await _client.PostAsync("/Tag/sync", null);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        var tags = JsonConvert.DeserializeObject<List<StackOverflowTag>>(content);
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    }
+
+    [Test]
+    public async Task Paged_EndpointReturnsSuccessAndCorrectNumbersData()
+    {
+        // Arrange
+        var sortPagingInfo = new SortPagingInfo
+        {
+            PageNumber = 1,
+            PageSize = 10,
+            OrderBy = "Name",
+            SortDirection = SortDirection.Ascending
+        };
+        var content = new StringContent(JsonConvert.SerializeObject(sortPagingInfo), Encoding.UTF8, "application/json");
+
+        //// Act
+        var response = await _client.PostAsync("/Tag/paged", content);
+
+        //// Assert
+        response.EnsureSuccessStatusCode();
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var pagedResult = JsonConvert.DeserializeObject<PagedCollectionOutDto<TagOutDto>>(responseContent);
+        
+        Assert.That("result", Is.Not.Null);
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        Assert.That(pagedResult?.PageNumber, Is.EqualTo(1));
+        Assert.That(pagedResult?.PageSize, Is.EqualTo(10));
+        Assert.That(pagedResult?.Collection.Count, Is.LessThanOrEqualTo(10));
+    }
+}
